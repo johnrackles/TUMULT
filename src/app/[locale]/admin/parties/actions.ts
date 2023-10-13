badd6af1281dd8aebe4574db8ccdd0d8164f91b8
John Rackles <john@rackles.io>
2023-10-13 10:57:06 +0200

    wip add party form

--- src/app/[locale]/admin/parties/actions.ts
+++ src/app/[locale]/admin/parties/actions.ts
@@ -4,7 +4,7 @@ import { utapi } from "@/app/api/uploadthing/api";
 import { authOptions } from "@/auth/auth";
 import { db } from "@/db/db";
 import { deleteImageSchema, images } from "@/db/image/schema";
-import { insertPartySchema, parties } from "@/db/party/schema";
+import { insertPartySchema } from "@/db/party/schema";
 import { eq } from "drizzle-orm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -13,7 +13,22 @@ import { type z } from "zod";
 export async function addParty(values: z.infer<typeof insertPartySchema>) {
   try {
     const validated = insertPartySchema.parse(values);
-    await db.insert(parties).values(validated);
+
+    // take the time and add it to the start date
+    const startTime = validated.beginTime
+      .split(":")
+      .map((s) => Number.parseInt(s, 10));
+
+    validated.begin.setHours(startTime[0]!, startTime[1]);
+
+    // take the time and add it to the end date
+    const endTime = validated.endTime
+      .split(":")
+      .map((s) => Number.parseInt(s, 10));
+
+    validated.end?.setHours(endTime[0]!, endTime[1]);
+    console.dir(validated);
+    //await db.insert(parties).values(validated);
   } catch (e) {
     if (e instanceof Error) {
       return { error: e.message };
