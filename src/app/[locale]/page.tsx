0c9eb2e040c24ba09af14db6c91bda1f6c73f435
Johannes Rackles <johannes.rackles@interone.de>
2024-06-21 15:24:45 +0200

    feat: new design

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,14 @@
-import { Logo } from "@/components/Logo";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="top-6ext-primary-foreground flex items-center justify-center bg-primary p-4">
-      <Logo className="w-3/4 max-w-lg" />
+    <main className="flex items-center justify-center bg-background p-4 text-primary">
+      <Image
+        className="w-3/4 max-w-lg"
+        src={require("@/assets/Tumult-wobbly_weiss.png")}
+        alt="TUMULT"
+        priority
+      />
     </main>
   );
 }
